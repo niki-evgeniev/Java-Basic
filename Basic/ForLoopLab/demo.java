fcfd5c925c047f3c5114699839375f3d7f944181
Nikolay Ivanov <niki.evgeniev@gmail.com>
2022-08-19 23:41:30 +0300

    Create demo.java

---
+++ Basic/ForLoopLab/demo.java
@@ -0,0 +1,35 @@
+package ForLoopLab;
+
+import java.util.Scanner;
+
+public class demo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        int sum = 0;
+        for (int i = 0; i <= text.length() - 1; i++) {
+            char symbol = text.charAt(i);
+            //буква	    a	e	i	o	u
+            //стойност	1	2	3	4	5
+            switch (symbol) {
+                case 'a':
+                    sum += 1;
+                    break;
+                case 'e':
+                    sum += 2;
+                    break;
+                case 'i':
+                    sum += 3;
+                    break;
+                case 'o':
+                    sum += 4;
+                    break;
+                case 'u':
+                    sum += 5;
+                    break;
+            }
+
+        }
+        System.out.println(sum);
+    }
+}
