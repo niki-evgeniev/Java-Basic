7fd5206309569ef17ad4b1c73b59c234e461cc84
Nikolay Ivanov <niki.evgeniev@gmail.com>
2022-08-19 23:42:06 +0300

    Create NumberSequence_08.java

---
+++ Basic/ForLoopLab/NumberSequence_08.java
@@ -0,0 +1,25 @@
+package ForLoopLab;
+
+import java.util.Scanner;
+
+public class NumberSequence_08 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+
+        for (int i = 1; i <= n; i++) {
+            int number = Integer.parseInt(scanner.nextLine());
+            if (max < number) {
+                max = number;
+            }
+            if (number < min) {
+                min = number;
+            }
+
+        }
+        System.out.printf("Max number: %d%nMin number: %d",max,min);
+
+    }
+}
