dbbedf76521c1c21a5e43dad249bd5eefb541c06
Nikolay Ivanov <niki.evgeniev@gmail.com>
2022-08-20 00:07:07 +0300

    Create Tringle.java

---
+++ Basic/Lab/Tringle.java
@@ -0,0 +1,16 @@
+package Lab;
+
+import javax.swing.*;
+import java.util.Scanner;
+
+public class Tringle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = Integer.parseInt(scanner.nextLine());
+        int b = Integer.parseInt(scanner.nextLine());
+        int sum = a * b;
+        System.out.println(sum);
+    }
+
+
+}
