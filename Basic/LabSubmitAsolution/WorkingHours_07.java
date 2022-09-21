27f8917b2537a30237f398cf3f6e86409172e944
Nikolay Ivanov <niki.evgeniev@gmail.com>
2022-09-22 00:49:26 +0300

    Create WorkingHours_07.java

---
+++ Basic/LabSubmitAsolution/WorkingHours_07.java
@@ -0,0 +1,31 @@
+package LabSubmitAsolution;
+
+import java.util.Scanner;
+
+public class WorkingHours_07 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int hour = Integer.parseInt(scanner.nextLine());
+        String day = scanner.nextLine();
+
+        if (hour >= 10 && hour <= 18) {
+            switch (day) {
+                case "Monday":
+                case "Tuesday":
+                case "Wednesday":
+                case "Thursday":
+                case "Friday":
+                case "Saturday":
+                    System.out.println("open");
+                    break;
+                case "Sunday":
+                    System.out.println("closed");
+            }
+
+        } else {
+            System.out.println("closed");
+        }
+    }
+
+}
+
