e37b44483178ac26a3bea77c0ef5e8cdf80bd03e
Nikolay Ivanov <niki.evgeniev@gmail.com>
2022-09-22 00:47:09 +0300

    Create test.java

---
+++ Basic/Lab/test.java
@@ -0,0 +1,16 @@
+package Lab;
+
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] args) {
+        Scanner Scanner = new Scanner(System.in);
+        double a = Double.parseDouble(Scanner.nextLine());
+        double h = Double.parseDouble(Scanner.nextLine());
+        double ah = a * h;
+        double ah2 = ah / 2;
+        System.out.println(ah2);
+
+
+    }
+}
