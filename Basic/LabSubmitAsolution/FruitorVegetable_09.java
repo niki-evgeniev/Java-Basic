cabf952081532986e6495eacb459377fd973d803
Nikolay Ivanov <niki.evgeniev@gmail.com>
2022-09-23 02:48:34 +0300

    Create FruitorVegetable_09.java

---
+++ Basic/LabSubmitAsolution/FruitorVegetable_09.java
@@ -0,0 +1,20 @@
+package LabSubmitAsolution;
+
+import java.util.Scanner;
+
+public class FruitorVegetable_09 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.next();
+
+        if (name.equals( "banana" ) || name.equals("apple") || name.equals("kiwi") || name.equals("cherry") || name.equals("lemon")|| name.equals("grapes")){
+            System.out.println("fruit");
+
+        }else if (name.equals("tomato") || name.equals("cucumber") || name.equals("pepper") || name.equals("carrot")){
+            System.out.println("vegetable");
+        }else {
+            System.out.println("unknown");
+        }
+
+    }
+}
