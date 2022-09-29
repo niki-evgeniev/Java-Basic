b340ca95ebf0bad10410a9491f845e0dcfb783df
Nikolay Ivanov <niki.evgeniev@gmail.com>
2022-09-30 02:03:05 +0300

    Create Combinations_03.java

---
+++ Basic/NestedLoopsLab/Combinations_03.java
@@ -0,0 +1,23 @@
+package NestedLoopsLab;
+
+import java.util.Scanner;
+
+public class Combinations_03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        int count =0;
+
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= n; j++) {
+                for (int k = 0; k <= n; k++) {
+                    int total = i + j + k;
+                    if (total == n){
+                    count++;
+                    }
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
